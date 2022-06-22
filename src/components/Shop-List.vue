48c2fc2d9c78cc3f35f9341658d61a5861ba8600
YottyHol <git@oholliday.co.uk>
2022-06-22 21:45:34 +0100

    buttons and checks

--- src/components/Shop-List.vue
+++ src/components/Shop-List.vue
@@ -13,22 +13,56 @@ export default {
 };
 </script>
 <template>
-  <OrderList v-model="products" listStyle="height:auto" dataKey="id">
-    <template #header> Shopping List </template>
-    <template #item="slotProps">
-      <div class="product-item">
-        <div class="image-container">
-          <img src="../assets/item.png" :alt="slotProps.item.name" />
+  <div>
+    <PVButton
+      class="new-button"
+      label="New Item"
+      icon="pi pi-plus"
+      iconPos="left"
+    />
+    <OrderList v-model="products" listStyle="height:auto" dataKey="id">
+      <template #header> Shopping List </template>
+      <template #item="slotProps">
+        <div class="product-item">
+          <div class="image-container">
+            <img src="../assets/item.png" alt="slotProps.item.name" />
+          </div>
+          <div class="product-list-detail">
+            <h2 class="mb-2">{{ slotProps.item.name }}</h2>
+          </div>
+          <div class="checkbox-container">
+            <h4 class="mb-2">Checked</h4>
+            <CheckBox :binary="true" v-model="slotProps.item.checked" />
+          </div>
+          <PVButton
+            class="p-button-danger p-button-sm delete-button"
+            icon="pi pi-times"
+            iconPos="left"
+          />
         </div>
-        <div class="product-list-detail">
-          <h2 class="mb-2">{{ slotProps.item.name }}</h2>
-        </div>
-      </div>
-    </template>
-  </OrderList>
+      </template>
+    </OrderList>
+  </div>
 </template>
 
 <style scoped>
+.checkbox-container {
+  text-align: center;
+}
+
+.new-button {
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: 70px;
+  width: calc(100% - 70px);
+}
+
+.delete-button {
+  margin-left: 2rem;
+  margin-top: 1rem;
+}
 .product-item {
   display: flex;
   align-items: center;
@@ -64,7 +98,11 @@ img {
   font-size: 0.875rem;
 }
 
-@media screen and (max-width: 576px) {
+@media screen and (max-width: 981px) {
+  .new-button {
+    margin: 2rem;
+  }
+
   .product-item {
     flex-wrap: wrap;
 
--- src/primeVue.js
+++ src/primeVue.js
@@ -1,15 +1,12 @@
 import PrimeVue from "primevue/config";
-import Menubar from "primevue/menubar";
-import Card from "primevue/card";
-import Panel from "primevue/panel";
 import OrderList from "primevue/orderlist";
+import Button from "primevue/button";
+import Checkbox from "primevue/checkbox";
 
 export default function main(app) {
   app.use(PrimeVue);
-  app.use(Card);
 
+  app.component("CheckBox", Checkbox);
+  app.component("PVButton", Button);
   app.component("OrderList", OrderList);
-  app.component("menu-bar", Menubar);
-  app.component("card-co", Card);
-  app.component("panel-co", Panel);
 }
